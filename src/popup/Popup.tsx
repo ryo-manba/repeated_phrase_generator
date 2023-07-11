d34328de9b8c7fc16f54d4e10c275c2b4a3ae0be
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-07-11 23:56:04 +0900

    ストレージを利用する

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,14 +1,39 @@
+import { ReactElement, useEffect, useState } from 'react';
 import { Container, Select } from '@mantine/core';
+import { getBucket } from '@extend-chrome/storage';
+
+interface MyBucket {
+  targetLang: string;
+}
+
+const bucket = getBucket<MyBucket>('my_bucket', 'sync');
 
 const Popup = () => {
   document.body.style.width = '20rem';
   document.body.style.height = '20rem';
 
+  const [style, setStyle] = useState('hiragana');
+
+  useEffect(() => {
+    (async () => {
+      const value = await bucket.get();
+      if (value.targetLang) {
+        setStyle(value.targetLang);
+      }
+    })();
+  }, []);
+
+  const saveStyle = (style: string) => {
+    bucket.set({ targetLang: style });
+    setStyle(style);
+  };
+
   return (
     <Container p="xl">
       <Select
         label="どのスタイルに変換しますか？"
-        defaultValue="hiragana"
+        value={style}
+        onChange={(value: string) => saveStyle(value)}
         data={[
           { value: 'hiragana', label: 'ひらがな' },
           { value: 'katakana', label: 'カタカナ' },
