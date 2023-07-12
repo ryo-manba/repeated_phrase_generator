b124ad29b197fd2a7dd8c6b8544b0d44707c8d5f
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-07-13 00:18:46 +0900

    セレクトボックスから言語を切り替える

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -14,6 +14,15 @@ import {
   Tooltip,
 } from '@mantine/core';
 import { useClickOutside } from '@mantine/hooks';
+import { generateRepeatedPhrase } from '../app/generate';
+import type { ConvertType } from '../app/generate';
+import { getBucket } from '@extend-chrome/storage';
+
+interface MyBucket {
+  targetStyle: string;
+}
+
+const bucket = getBucket<MyBucket>('my_bucket', 'sync');
 
 type ContentProps = {
   generatedText: string;
@@ -24,11 +33,21 @@ type ContentProps = {
 const Content = ({ generatedText, originalText, targetStyle }: ContentProps) => {
   const [opened, setOpened] = useState(true);
   const [diaglog, setDialog] = useState<HTMLDivElement | null>(null);
+  const [text, setText] = useState(generatedText);
+  const [style, setStyle] = useState(targetStyle);
 
   //  外部クリックが行われた場合にダイアログを閉じるためのフックを使用する
   useClickOutside(() => setOpened(false), null, [diaglog]);
 
+  // TODO: iconを作成する
   const IconUrl = chrome.runtime.getURL('images/extension_128.png');
+
+  const handleChange = async (value: string) => {
+    bucket.set({ targetStyle: value });
+    const newText = await generateRepeatedPhrase(originalText, value as ConvertType);
+    setText(newText);
+    setStyle(value);
+  };
 
   if (!opened) return null;
   return (
@@ -49,7 +68,8 @@ const Content = ({ generatedText, originalText, targetStyle }: ContentProps) => 
         <Avatar src={IconUrl} />
         <Text size="md">変換結果：</Text>
         <Select
-          value={targetStyle}
+          value={style}
+          onChange={(value: string) => handleChange(value)}
           size="xs"
           data={[
             { value: 'hiragana', label: 'ひらがな' },
@@ -59,7 +79,7 @@ const Content = ({ generatedText, originalText, targetStyle }: ContentProps) => 
       </Flex>
       <Divider />
       <Stack pt="sm" spacing="xs" style={{ textAlign: 'left' }}>
-        <Text size="sm">{generatedText}</Text>
+        <Text size="sm">{text}</Text>
         <Group position="right" spacing="xs">
           {/* 音声読み上げアイコンを表示する（未実装） */}
           <Tooltip label="音声読み上げ" withArrow>
@@ -68,7 +88,7 @@ const Content = ({ generatedText, originalText, targetStyle }: ContentProps) => 
             </ActionIcon>
           </Tooltip>
           {/* テキストをコピーするボタンを表示する */}
-          <CopyButton value={generatedText}>
+          <CopyButton value={text}>
             {({ copied, copy }) => (
               <Tooltip label={copied ? 'コピーしました' : 'クリップボードにコピー'} withArrow>
                 <ActionIcon onClick={copy}>
